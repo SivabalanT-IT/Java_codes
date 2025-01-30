56e5ace43b8bca6c88d8878e2ff538e6fdea0002
SivabalanT-IT <sivabalant04@gmail.com>
2025-01-30 20:23:16 +0530

    Create LetterComparison.java

---
+++ LetterComparison.java
@@ -0,0 +1,14 @@
+public class LetterComparison {
+    public static void main(String[] args) {
+        char letter1 = 'A';
+        char letter2 = 'B';
+
+        // Comparison operations
+        System.out.println(letter1 + " < " + letter2 + ": " + (letter1 < letter2));  
+        System.out.println(letter1 + " <= " + letter2 + ": " + (letter1 <= letter2));  
+        System.out.println(letter1 + " > " + letter2 + ": " + (letter1 > letter2));  
+        System.out.println(letter1 + " >= " + letter2 + ": " + (letter1 >= letter2));  
+        System.out.println(letter1 + " == " + letter2 + ": " + (letter1 == letter2));  
+        System.out.println(letter1 + " != " + letter2 + ": " + (letter1 != letter2));  
+    }
+}
